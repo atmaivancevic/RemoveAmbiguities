73a023020fc593f7f85da1279339d5d4bf49edec
Atma <atma.ivancevic@adelaide.edu.au>
2015-01-14 15:32:28 +1030

    made better

--- Remove_nucleotide_ambiguities.sh
+++ Remove_nucleotide_ambiguities.sh
@@ -1,19 +1,22 @@
-# Remove nucleotide ambiguities (e.g. from file L1_query_seqs_PLANTS_and_ANIMALS.fasta)
+# Remove nucleotide ambiguities (e.g. from file seq1.fa)
+
+#$1 = name of sequence (e.g. seq1)
+
 
 # Ignore header lines (i.e. lines starting with '>')
 # Feed into the command line one line at a time
-sed '/^>/!s/M/N/g' L1_query_seqs_PLANTS_and_ANIMALS.fasta >> L1_query_seqs_PLANTS_and_ANIMALS_M.fasta
-sed '/^>/!s/R/N/g' L1_query_seqs_PLANTS_and_ANIMALS_M.fasta >> L1_query_seqs_PLANTS_and_ANIMALS_R.fasta
-sed '/^>/!s/W/N/g' L1_query_seqs_PLANTS_and_ANIMALS_R.fasta >> L1_query_seqs_PLANTS_and_ANIMALS_W.fasta
-sed '/^>/!s/S/N/g' L1_query_seqs_PLANTS_and_ANIMALS_W.fasta >> L1_query_seqs_PLANTS_and_ANIMALS_S.fasta
-sed '/^>/!s/Y/N/g' L1_query_seqs_PLANTS_and_ANIMALS_S.fasta >> L1_query_seqs_PLANTS_and_ANIMALS_Y.fasta
-sed '/^>/!s/K/N/g' L1_query_seqs_PLANTS_and_ANIMALS_Y.fasta >> L1_query_seqs_PLANTS_and_ANIMALS_K.fasta
-sed '/^>/!s/V/N/g' L1_query_seqs_PLANTS_and_ANIMALS_K.fasta >> L1_query_seqs_PLANTS_and_ANIMALS_V.fasta
-sed '/^>/!s/H/N/g' L1_query_seqs_PLANTS_and_ANIMALS_V.fasta >> L1_query_seqs_PLANTS_and_ANIMALS_H.fasta
-sed '/^>/!s/D/N/g' L1_query_seqs_PLANTS_and_ANIMALS_H.fasta >> L1_query_seqs_PLANTS_and_ANIMALS_D.fasta
-sed '/^>/!s/B/N/g' L1_query_seqs_PLANTS_and_ANIMALS_D.fasta >> L1_query_seqs_PLANTS_and_ANIMALS_B.fasta
-sed '/^>/!s/X/N/g' L1_query_seqs_PLANTS_and_ANIMALS_B.fasta >> L1_query_seqs_PLANTS_and_ANIMALS_X.fasta
+sed '/^>/!s/M/N/g' $1.fa >> $1_M.fa
+sed '/^>/!s/R/N/g' $1_M.fa >> $1_R.fa
+sed '/^>/!s/W/N/g' $1_R.fa >> $1_W.fa
+sed '/^>/!s/S/N/g' $1_W.fa >> $1_S.fa
+sed '/^>/!s/Y/N/g' $1_S.fa >> $1_Y.fa
+sed '/^>/!s/K/N/g' $1_Y.fa >> $1_K.fa
+sed '/^>/!s/V/N/g' $1_K.fa >> $1_V.fa
+sed '/^>/!s/H/N/g' $1_V.fa >> $1_H.fa
+sed '/^>/!s/D/N/g' $1_H.fa >> $1_D.fa
+sed '/^>/!s/B/N/g' $1_D.fa >> $1_B.fa
+sed '/^>/!s/X/N/g' $1_B.fa >> $1_X.fa
 
-rm L1_query_seqs_PLANTS_and_ANIMALS.fasta
-mv L1_query_seqs_PLANTS_and_ANIMALS_X.fasta L1_query_seqs_PLANTS_and_ANIMALS.fasta
-rm L1_query_seqs_PLANTS_and_ANIMALS_*.fasta
+rm $1.fa
+mv $1_X.fa $1.fa
+rm $1_*.fa
